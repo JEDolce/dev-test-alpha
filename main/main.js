74cb8dd88b667ecabc90528160e7a421c73833ce
jedolce <juandolce20@gmail.com>
2023-10-13 15:50:33 -0300

    end basic intro section mobile

--- main/main.js
+++ main/main.js
@@ -26,4 +26,40 @@ btn.addEventListener("click", () => {
     }
 });
 
+
+// ********** Countdown ************
+(function () {
+    const second = 1000,
+        minute = second * 60,
+        hour = minute * 60,
+        day = hour * 24;
+
+    countDown = new Date(2024, 1, 1, 18, 0, 0).getTime(),
+        x = setInterval(function () {
+
+            let now = new Date().getTime(),
+                distance = countDown - now;
+
+            document.getElementById("days").innerText = Math.floor(distance / (day)),
+                document.getElementById("hours").innerText = Math.floor((distance % (day)) / (hour)),
+                document.getElementById("minutes").innerText = Math.floor((distance % (hour)) / (minute)),
+                document.getElementById("seconds").innerText = Math.floor((distance % (minute)) / second);
+
+            //do something later when date is reached
+            if (distance < 0) {
+                let headline = document.getElementById("headline"),
+                    section = document.getElementById("section"),
+                    countdown = document.getElementById("countdown");
+
+                headline.innerText = "EMPEZO LA FIESTA!!!";
+                section.style.height = "6rem"
+                countdown.style.display = "none";
+
+                clearInterval(x);
+            }
+        }, 0)
+}());
+
+
+
 // ********** FAQ menu ************
